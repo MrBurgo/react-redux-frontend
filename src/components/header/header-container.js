2c344475285367fde4c723d397d9d5e94eddcd75
Nathan Burgo <nthnbrg185@gmail.com>
2018-05-24 15:34:12 -0600

    Some more header refactoring

--- src/components/header/header-container.js
+++ src/components/header/header-container.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import './header-container.css'
 
 class Header extends React.Component {
+  onLogoutClick = (event) => {
+    event.preventDefault()
+    // this.props.logout()
+  }
+
   render() {
     return (
       <div className='header-container'>
         <a className='title' href='#title'>Stupid To-Dos</a>
+        { this.props.username !== null && <a href="#logout" onClick={this.onLogoutClick}>{`Logout ${this.props.username}`}</a>}
       </div>
     )
   }
 }
 
-export default Header
+const mapStateToProps = (state) => state.auth
+// const mapDispatchToProps = (dispatch) => bindActionCreators({logout}, dispatch)
+
+export default connect(mapStateToProps)(Header)
