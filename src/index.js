dba423b280d714059aa57b5a85fa258a419390c3
Nathan Burgo <nthnbrg185@gmail.com>
2018-05-24 15:15:45 -0600

    Bunch of redux stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch } from 'react-router-dom'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reducers from './reducers/root-reducer'
 import Header from './components/header/header-container'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
 
-ReactDOM.render(
-  (
+ReactDOM.render((
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Header />
       </div>
     </BrowserRouter>
+  </Provider>
 ), document.getElementById('root'))
 registerServiceWorker()
