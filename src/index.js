f147b5ea46c71add45ade80092809b113b93bbcd
Nathan Burgo <nthnbrg185@gmail.com>
2018-05-24 14:41:14 -0600

    Preparing to toast app.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch } from 'react-router-dom'
+import Header from './components/header/header-container'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+  (
+    <BrowserRouter>
+      <div>
+        <Header />
+      </div>
+    </BrowserRouter>
+), document.getElementById('root')
+)
+registerServiceWorker()
