284ae0af524093cc910ebf04ac3698b8fec740f3
Nathan Burgo <nthnbrg185@gmail.com>
2018-05-24 16:33:36 -0600

    Re-enabled commented code and added cases in reducer

--- src/reducers/auth-reducer.js
+++ src/reducers/auth-reducer.js
@@ -1,3 +1,5 @@
+import { LOGIN_FAIL, LOGIN_PASS } from '../actions/auth-actions'
+
 function AuthReducer(state = null, action) {
   const initialState = {
     username: null,
@@ -7,6 +9,20 @@ function AuthReducer(state = null, action) {
 
   if (state) {
     switch (action.type) {
+      case LOGIN_PASS:
+        return {
+          ...state,
+          username: action.payload.username,
+          token: action.payload.token,
+          error: null
+        }
+      case LOGIN_FAIL:
+        return {
+          ...state,
+          username: null,
+          token: null,
+          error: action.payload.error
+        }
       default:
         return state
     }
